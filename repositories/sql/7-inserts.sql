6769ddb25b17d906ee0ec76f60e44f990b84c990
ChristianMinich <119011619+ChristianMinich@users.noreply.github.com>
2023-05-23 18:45:35 +0200

    sql bugs

--- repositories/sql/7-inserts.sql
+++ repositories/sql/7-inserts.sql
@@ -1,50 +1,49 @@
 USE QUIZDUELL;
 
-#USERNAME
-INSERT INTO USER VALUES (1, 'sadik.slimshady', 'pass12345', 100, 10);
-INSERT INTO USER VALUES (2, 'jan-detti', 'password123465', 50, 10);
-INSERT INTO USER VALUES (3, 'frauchraih', 'miaumiau789', 10, 20);
-INSERT INTO USER VALUES (4, 'drachy.minich', 'max187187', 170, 20);
-INSERT INTO USER VALUES (5, '_tonyx_', 'hubsch-88', 5, 10);
-
-
-#QUESTION
-INSERT INTO QUESTION VALUES (1, 'Wann wurde der Campus in Lingen errichtet?', '2012', '2009', '2005', '2014', 1);
-INSERT INTO QUESTION VALUES (2, 'Welches ist das Wahrzeichen von Lingen(Ems)?', 'Ein Turm der Stadtmauer', 'Ein Leuchtturm', 'Ein Turm der Stadtmauer', 'Ein Schloss', 2);
-INSERT INTO QUESTION VALUES (3, 'Wie viele Einwohner hat Lingen(Ems)?', '50.000', '20.000', '80.000', '120.000', 3);
-INSERT INTO QUESTION VALUES (4, 'Was bedeutet das Kürzel "HTML"?', 'Hyperlink Text Markup Language', 'High Tech Management Library', 'Human Terminal Management Logic', 'Hardware Transmission Multiplexing Layer', 2);
-INSERT INTO QUESTION VALUES (5, 'Wie viele Beine hatte Jesus?', '3', '2', 'Man weiß es nicht ganz genau', '5', 3);
-INSERT INTO QUESTION VALUES (6, 'Was ist eine "Shell"?', 'Eine Texteingabeaufforderung, die Befehle ausführt', 'Eine grafische Benutzeroberfläche für Betriebssysteme', 'Eine Software zum Schutz von Computern vor Viren', 'Eine Bibliothek mit Grafikfunktionen zur Erstellung von 3D-Animationen', 2);
-INSERT INTO QUESTION VALUES (7, 'Würde Rauscher gute Noten verteilen?', 'Ja wie meine Mutter', 'Nein, maximal 4.0', 'Nein', 'Nein', 3);
-INSERT INTO QUESTION VALUES (8, 'Was ist ein Turing-Test?', 'Ein Test zur Messung der Fähigkeit eines Computers, menschliches Verhalten zu imitieren', 'Ein Test zur Messung der Intelligenz eines Menschen', 'Ein Test zur Messung der Leistung eines Computers', 'Ein Test zur Messung der Fähigkeit eines Menschen, Programmiersprachen zu erlernen', 2);
-INSERT INTO QUESTION VALUES (9, 'Wie viele Tasten hat eine durchschnittliche Computermaus?', '10 Tasten', '1 Taste', '3 Tasten', '2 Tasten', 2);
-INSERT INTO QUESTION VALUES (10, 'Was bedeutet das Kürzel "HTTP"?', 'Hypertext Transmission Protocol', 'Hyper Transfer Text Programming', 'Human Terminal Transaction Platform', 'Hardware Transmission Telephony Process', 2);
-
-#GAME
-INSERT INTO GAME VALUES (100,1, 1 );
-INSERT INTO GAME VALUES (100,4, 1 );
-INSERT INTO GAME VALUES (200,2, 2);
-INSERT INTO GAME VALUES (200,5, 2);
-INSERT INTO GAME VALUES (300,3, 3);
-INSERT INTO GAME VALUES (300,1, 3);
-
 #AVATAR
-INSERT INTO AVATAR VALUES (10, NULL);
-INSERT INTO AVATAR VALUES (20, NULL);
+INSERT INTO AVATAR (FILE_PATH) VALUES (NULL);
+INSERT INTO AVATAR (FILE_PATH) VALUES (NULL);
 
-#BELONG
-INSERT INTO BELONG VALUES (1, 100);
-INSERT INTO BELONG VALUES (2, 100);
-INSERT INTO BELONG VALUES (3, 100);
-INSERT INTO BELONG VALUES (4, 200);
-INSERT INTO BELONG VALUES (5, 300);
-INSERT INTO BELONG VALUES (6, 200);
-INSERT INTO BELONG VALUES (7, 100);
-INSERT INTO BELONG VALUES (8, 200);
-INSERT INTO BELONG VALUES (9, 200);
-INSERT INTO BELONG VALUES (10, 200);
+#USERNAME
+INSERT INTO USER (USERNAME, PASSWORD, HIGHSCORE, AVATAR_ID) VALUES ('sadik.slimshady', 'pass12345', 100, 1);
+INSERT INTO USER (USERNAME, PASSWORD, HIGHSCORE, AVATAR_ID) VALUES ('jan-detti', 'password123465', 50, 1);
+INSERT INTO USER (USERNAME, PASSWORD, HIGHSCORE, AVATAR_ID) VALUES ('frauchraih', 'miaumiau789', 10, 1);
+INSERT INTO USER (USERNAME, PASSWORD, HIGHSCORE, AVATAR_ID) VALUES ('drachy.minich', 'max187187', 170, 1);
+INSERT INTO USER (USERNAME, PASSWORD, HIGHSCORE, AVATAR_ID) VALUES ('_tonyx_', 'hubsch-88', 5, 1);
 
 #CATEGORY
-INSERT INTO CATEGORY VALUES (1, 'Hochschule');
-INSERT INTO CATEGORY VALUES (2, 'Informatik');
-INSERT INTO CATEGORY VALUES (3, 'Random');
+INSERT INTO CATEGORY (CATEGORY) VALUES ('Hochschule');
+INSERT INTO CATEGORY (CATEGORY) VALUES ('Informatik');
+INSERT INTO CATEGORY (CATEGORY) VALUES ('Random');
+
+#QUESTION
+INSERT INTO QUESTION (QUESTION, RIGHT_ANSWER, FALSE_ANSWER1, FALSE_ANSWER2, FALSE_ANSWER3, CATEGORY_ID) VALUES ('Wann wurde der Campus in Lingen errichtet?', '2012', '2009', '2005', '2014', 1);
+INSERT INTO QUESTION (QUESTION, RIGHT_ANSWER, FALSE_ANSWER1, FALSE_ANSWER2, FALSE_ANSWER3, CATEGORY_ID) VALUES ('Welches ist das Wahrzeichen von Lingen(Ems)?', 'Ein Turm der Stadtmauer', 'Ein Leuchtturm', 'Ein Turm der Stadtmauer', 'Ein Schloss', 2);
+INSERT INTO QUESTION (QUESTION, RIGHT_ANSWER, FALSE_ANSWER1, FALSE_ANSWER2, FALSE_ANSWER3, CATEGORY_ID) VALUES ('Wie viele Einwohner hat Lingen(Ems)?', '50.000', '20.000', '80.000', '120.000', 3);
+INSERT INTO QUESTION (QUESTION, RIGHT_ANSWER, FALSE_ANSWER1, FALSE_ANSWER2, FALSE_ANSWER3, CATEGORY_ID) VALUES ('Was bedeutet das Kürzel "HTML"?', 'Hyperlink Text Markup Language', 'High Tech Management Library', 'Human Terminal Management Logic', 'Hardware Transmission Multiplexing Layer', 2);
+INSERT INTO QUESTION (QUESTION, RIGHT_ANSWER, FALSE_ANSWER1, FALSE_ANSWER2, FALSE_ANSWER3, CATEGORY_ID) VALUES ('Wie viele Beine hatte Jesus?', '3', '2', 'Man weiß es nicht ganz genau', '5', 3);
+INSERT INTO QUESTION (QUESTION, RIGHT_ANSWER, FALSE_ANSWER1, FALSE_ANSWER2, FALSE_ANSWER3, CATEGORY_ID) VALUES ('Was ist eine "Shell"?', 'Eine Texteingabeaufforderung, die Befehle ausführt', 'Eine grafische Benutzeroberfläche für Betriebssysteme', 'Eine Software zum Schutz von Computern vor Viren', 'Eine Bibliothek mit Grafikfunktionen zur Erstellung von 3D-Animationen', 2);
+INSERT INTO QUESTION (QUESTION, RIGHT_ANSWER, FALSE_ANSWER1, FALSE_ANSWER2, FALSE_ANSWER3, CATEGORY_ID) VALUES ('Würde Rauscher gute Noten verteilen?', 'Ja wie meine Mutter', 'Nein, maximal 4.0', 'Nein', 'Nein', 3);
+INSERT INTO QUESTION (QUESTION, RIGHT_ANSWER, FALSE_ANSWER1, FALSE_ANSWER2, FALSE_ANSWER3, CATEGORY_ID) VALUES ('Was ist ein Turing-Test?', 'Ein Test zur Messung der Fähigkeit eines Computers, menschliches Verhalten zu imitieren', 'Ein Test zur Messung der Intelligenz eines Menschen', 'Ein Test zur Messung der Leistung eines Computers', 'Ein Test zur Messung der Fähigkeit eines Menschen, Programmiersprachen zu erlernen', 2);
+INSERT INTO QUESTION (QUESTION, RIGHT_ANSWER, FALSE_ANSWER1, FALSE_ANSWER2, FALSE_ANSWER3, CATEGORY_ID) VALUES ('Wie viele Tasten hat eine durchschnittliche Computermaus?', '10 Tasten', '1 Taste', '3 Tasten', '2 Tasten', 2);
+INSERT INTO QUESTION (QUESTION, RIGHT_ANSWER, FALSE_ANSWER1, FALSE_ANSWER2, FALSE_ANSWER3, CATEGORY_ID) VALUES ('Was bedeutet das Kürzel "HTTP"?', 'Hypertext Transmission Protocol', 'Hyper Transfer Text Programming', 'Human Terminal Transaction Platform', 'Hardware Transmission Telephony Process', 2);
+
+#GAME
+INSERT INTO GAME (USER_ID, CATEGORY_ID) VALUES (1, 1);
+INSERT INTO GAME (USER_ID, CATEGORY_ID) VALUES (4, 1);
+INSERT INTO GAME (USER_ID, CATEGORY_ID) VALUES (2, 2);
+INSERT INTO GAME (USER_ID, CATEGORY_ID) VALUES (5, 2);
+INSERT INTO GAME (USER_ID, CATEGORY_ID) VALUES (3, 3);
+INSERT INTO GAME (USER_ID, CATEGORY_ID) VALUES (1, 3);
+
+#BELONG
+INSERT INTO BELONG (QUESTION_ID, GAME_ID) VALUES (1, 100);
+INSERT INTO BELONG (QUESTION_ID, GAME_ID) VALUES (2, 100);
+INSERT INTO BELONG (QUESTION_ID, GAME_ID) VALUES (3, 100);
+INSERT INTO BELONG (QUESTION_ID, GAME_ID) VALUES (4, 200);
+INSERT INTO BELONG (QUESTION_ID, GAME_ID) VALUES (5, 300);
+INSERT INTO BELONG (QUESTION_ID, GAME_ID) VALUES (6, 200);
+INSERT INTO BELONG (QUESTION_ID, GAME_ID) VALUES (7, 100);
+INSERT INTO BELONG (QUESTION_ID, GAME_ID) VALUES (8, 200);
+INSERT INTO BELONG (QUESTION_ID, GAME_ID) VALUES (9, 200);
+INSERT INTO BELONG (QUESTION_ID, GAME_ID) VALUES (10, 200);
