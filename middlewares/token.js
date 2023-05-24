998cfefbb4d8790689011677f37d40387c7f2c32
ChristianMinich <119011619+ChristianMinich@users.noreply.github.com>
2023-05-24 19:00:38 +0200

    code style

--- middlewares/token.js
+++ middlewares/token.js
@@ -1,26 +1,25 @@
-
 const svc = require("../services");
 
 function validate(req, res, next) {
-    const token = req.cookies["accessToken"]
-    if (token === undefined) {
-        res.redirect("/login");
-        return;
+  const token = req.cookies["accessToken"];
+  if (token === undefined) {
+    res.redirect("/login");
+    return;
+  }
+  try {
+    let tokenData = svc.getData(token);
+    if (tokenData) {
+      res.locals.tokenData = tokenData;
+      next();
+    } else {
+      res.status(401).json({ message: "authentication error" });
     }
-    try {
-        let tokenData = svc.getData(token);
-        if (tokenData) {
-            res.locals.tokenData = tokenData;
-            next();
-        } else {
-            res.status(401).json({message: "authentication error"});
-        }
-    } catch (e) {
-        res.status(401).json({
-            message: "authentication error: ",
-            error: e.message
-        });
-    }
+  } catch (e) {
+    res.status(401).json({
+      message: "authentication error: ",
+      error: e.message,
+    });
+  }
 }
 
-module.exports = {validate};
+module.exports = { validate };
--- routes/auth.js
+++ routes/auth.js
@@ -1,180 +1,198 @@
 const express = require("express");
 const router = express.Router();
-const path = require('path');
-const svc = require('../services');
-const database = require('../repositories/index');
+const path = require("path");
+const svc = require("../services");
+const database = require("../repositories/index");
 const db = database.getConnection();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-router.get('/', (req, res) => {
-    res.status(200).sendFile(path.resolve('public/index.html'));
-    try{
-        if (isJWT(req.cookies.accessToken)){
-        const data = svc.getData(req.cookies.accessToken);
+router.get("/", (req, res) => {
+  res.status(200).sendFile(path.resolve("public/index.html"));
+  try {
+    if (isJWT(req.cookies.accessToken)) {
+      const data = svc.getData(req.cookies.accessToken);
 
-        res.render('dashboard', {user: data.username });
-        }
-    }catch (error){
-        console.log(error);
+      res.render("dashboard", { user: data.username });
     }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-router.get('/index', (req, res) => {
-    res.status(200).redirect('/');
+router.get("/index", (req, res) => {
+  res.status(200).redirect("/");
 });
 
-router.get('/login', (req, res) => {
-    res.status(200).sendFile(path.resolve('public/login.html'));
+router.get("/login", (req, res) => {
+  res.status(200).sendFile(path.resolve("public/login.html"));
 });
 
-router.get('/logout', (req, res) => {
-    res.clearCookie("accessToken");
-    res.status(200).redirect('/login');
+router.get("/logout", (req, res) => {
+  res.clearCookie("accessToken");
+  res.status(200).redirect("/login");
 });
 
-router.get('/game-test', (req, res) => {
-    res.render('game.ejs');
+router.get("/game-test", (req, res) => {
+  res.render("game.ejs");
 });
 
-router.get('/scoreboard', (req, res) => {
-    res.status(200).sendFile(path.resolve('public/scoreboard.html'));
+router.get("/scoreboard", (req, res) => {
+  res.status(200).sendFile(path.resolve("public/scoreboard.html"));
 });
 
-router.get('/register', (req, res) => {
-    res.status(200).sendFile(path.resolve('public/register.html'));
+router.get("/register", (req, res) => {
+  res.status(200).sendFile(path.resolve("public/register.html"));
 });
 
-router.get('/game', (req, res) => {
-    res.sendFile(path.resolve('public/game.html'));
+router.get("/game", (req, res) => {
+  res.sendFile(path.resolve("public/game.html"));
 });
 
 router.post("/api/auth", (req, res) => {
-    const {username, password} = req.body;
-    db.then(conn => {
-        conn.query('SELECT USERNAME, PASSWORD FROM USER WHERE USERNAME = ?', [username])
-        .then(rows => {
-            console.log(rows);
-            if(rows.length === 0) res.status(401).send('Wrong Login Credentials!');
-            bcrypt.compare(password, rows[0].PASSWORD)
-            .then((valid) => {
-                console.log(valid);
-                if(valid && rows[0].USERNAME == username){
+  const { username, password } = req.body;
+  db.then((conn) => {
+    conn
+      .query("SELECT USERNAME, PASSWORD FROM USER WHERE USERNAME = ?", [
+        username,
+      ])
+      .then((rows) => {
+        console.log(rows);
+        if (rows.length === 0) res.status(401).send("Wrong Login Credentials!");
+        bcrypt
+          .compare(password, rows[0].PASSWORD)
+          .then((valid) => {
+            console.log(valid);
+            if (valid && rows[0].USERNAME == username) {
+              conn
+                .query("SELECT USER_ID FROM USER WHERE USERNAME = ?", [
+                  username,
+                ])
+                .then((rows) => {
+                  console.log(rows);
+                  try {
+                    if (rows.length !== 0) {
+                      let id = rows[0].USER_ID;
 
-                    conn.query('SELECT USER_ID FROM USER WHERE USERNAME = ?', [username])
-                    .then(rows => {
-                        console.log(rows);
-                        try{
-                            if(rows.length !== 0){
-    
-                                let id = rows[0].USER_ID
-    
-                                const token = jwt.sign({id, username}, process.env.JWT_SECRET, {
-                                    algorithm: "HS256",
-                                    expiresIn: "12h",
-                                    subject: "auth_token"
-                                });
-    
-                                res.cookie("accessToken", token, {httpOnly: false});
-                                res.status(200).redirect('/');
-                            } else {
-                                res.status(400).send("An Error has Occured!");
-                            }
-                                
-                            } catch (error) {
-                                console.log(error);
-                            }
-                    })
+                      const token = jwt.sign(
+                        { id, username },
+                        process.env.JWT_SECRET,
+                        {
+                          algorithm: "HS256",
+                          expiresIn: "12h",
+                          subject: "auth_token",
+                        }
+                      );
 
-                } else {
-                    res.status(400).send("Invalid Request");
-                }
-            }).catch(error => {
-                res.status(403).send(error);
-            })
-        }).catch(error => {
-            console.log(error);
-        })
-    })
+                      res.cookie("accessToken", token, { httpOnly: false });
+                      res.status(200).redirect("/");
+                    } else {
+                      res.status(400).send("An Error has Occured!");
+                    }
+                  } catch (error) {
+                    console.log(error);
+                  }
+                });
+            } else {
+              res.status(400).send("Invalid Request");
+            }
+          })
+          .catch((error) => {
+            res.status(403).send(error);
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 });
 
 router.post("/api/auth/register", (req, res) => {
-    const {username, password} = req.body;
-    db.then(conn => {
-        conn.query('SELECT USERNAME FROM USER WHERE USERNAME = ?', [username])
-        .then(rows => {
-            try{
-            if(rows[0].USERNAME !== 0) {
-                return res.send("User already exists");
-            }
-            } catch (error){
-            
-            let hashedPW = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  const { username, password } = req.body;
+  db.then((conn) => {
+    conn
+      .query("SELECT USERNAME FROM USER WHERE USERNAME = ?", [username])
+      .then((rows) => {
+        try {
+          if (rows[0].USERNAME !== 0) {
+            return res.send("User already exists");
+          }
+        } catch (error) {
+          let hashedPW = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
 
-            conn.query('INSERT INTO USER (USERNAME, PASSWORD, HIGHSCORE, AVATAR_ID) VALUES (?, ?, ?, ?)', [username, hashedPW, 0, 1])
-            .then(rows => {
-                console.log(rows);
-                conn.query('SELECT USER_ID FROM USER WHERE USERNAME = ?', [username])
-                .then(rows => {
-                    
-                    try{
-                        if(rows.length !== 0){
+          conn
+            .query(
+              "INSERT INTO USER (USERNAME, PASSWORD, HIGHSCORE, AVATAR_ID) VALUES (?, ?, ?, ?)",
+              [username, hashedPW, 0, 1]
+            )
+            .then((rows) => {
+              console.log(rows);
+              conn
+                .query("SELECT USER_ID FROM USER WHERE USERNAME = ?", [
+                  username,
+                ])
+                .then((rows) => {
+                  try {
+                    if (rows.length !== 0) {
+                      let id = rows[0].USER_ID;
 
-                            let id = rows[0].USER_ID
-
-                            const token = jwt.sign({id, username}, process.env.JWT_SECRET, {
-                                algorithm: "HS256",
-                                expiresIn: "12h",
-                                subject: "auth_token"
-                            });
-
-                            res.cookie("accessToken", token, {httpOnly: false});
-                            res.status(200).redirect('/');
-                        } else {
-                            res.status(400).send("An Error has Occured!");
+                      const token = jwt.sign(
+                        { id, username },
+                        process.env.JWT_SECRET,
+                        {
+                          algorithm: "HS256",
+                          expiresIn: "12h",
+                          subject: "auth_token",
                         }
-                            
-                        } catch (error) {
-                            console.log(error);
-                        }
-                })
+                      );
+
+                      res.cookie("accessToken", token, { httpOnly: false });
+                      res.status(200).redirect("/");
+                    } else {
+                      res.status(400).send("An Error has Occured!");
+                    }
+                  } catch (error) {
+                    console.log(error);
+                  }
+                });
             })
-            .catch(error => {
-                console.log(error);
-                res.status(400).send(error);
-            })
+            .catch((error) => {
+              console.log(error);
+              res.status(400).send(error);
+            });
         }
-        })
-    })
-
+      });
+  });
 });
 
-router.get('/scoreboard', (req, res) => {
-      db.then(conn => {
-        conn.query('SELECT USERNAME, HIGHSCORE FROM USER ORDER BY HIGHSCORE LIMIT 10')
-          .then(rows => {
-            res.json(rows);
-            conn.end();
-          })
-          .catch(error => {
-            console.log(error);
-            conn.end();
-            res.status(500).json({ error: 'Internal Server Error' });
-          });
+router.get("/scoreboard", (req, res) => {
+  db.then((conn) => {
+    conn
+      .query("SELECT USERNAME, HIGHSCORE FROM USER ORDER BY HIGHSCORE LIMIT 10")
+      .then((rows) => {
+        res.json(rows);
+        conn.end();
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+        conn.end();
+        res.status(500).json({ error: "Internal Server Error" });
       });
+  }).catch((error) => {
+    console.log(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  });
 });
 
 const isJWT = (token) => {
-    if (typeof token !== 'string' || !token) {
-      return false;
-    }
-  
-    const tokenParts = token.split('.');
-    return tokenParts.length === 3 && tokenParts[0] && tokenParts[1] && tokenParts[2];
-  };  
+  if (typeof token !== "string" || !token) {
+    return false;
+  }
+
+  const tokenParts = token.split(".");
+  return (
+    tokenParts.length === 3 && tokenParts[0] && tokenParts[1] && tokenParts[2]
+  );
+};
 
 module.exports = router;
