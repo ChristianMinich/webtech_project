99fa6f28d5e52f62d40fa719e827eb3acfa61552
ChristianMinich <chris.minich.cm@gmail.com>
2023-06-01 10:49:50 +0200

    added database highscore

--- game/QuizGame.js
+++ game/QuizGame.js
@@ -1,18 +1,19 @@
 const qs = require("../repositories/questions");
+const database = require("../repositories");
+const db = database.getConnection();
+class QuizGame {
 
-class QuizGame { 
-  
   constructor(roomId, io) {
     this.MAX_ROUNDS = 5;
     this.io = io;
     this.roomId = roomId;
-    this.questions = []; 
+    this.questions = [];
     this.currentQuestionIndex = 0;
     this.currentRightAnswer = "";
-    this.round = 1; 
+    this.round = 1;
     this.players = [];
-    console.log("new game created " + roomId );
-    
+    console.log("new game created " + roomId);
+
   }
   start() {
 
@@ -20,7 +21,7 @@ class QuizGame {
     //this.players = players;
     this.currentQuestionIndex = 0;
     console.log("Spiel startet");
-    
+
     getNextQuestion().then(question => {
       console.log("Runde :" + this.round + " " + question.text);
       this.sendQuestion(question, this.round);
@@ -43,7 +44,7 @@ class QuizGame {
     console.log("Neuer log " + String(this.players[this.players.length - 1].username));
   }
   sendQuestion(question) {
-     
+
     if (question) {
       this.io.to(this.roomId).emit('question', question, this.round);
     } else {
@@ -52,16 +53,16 @@ class QuizGame {
   }
 
   answerQuestion(username, answer) {
-  
-    console.log("Room: " + this.roomId +" | User: " + username + " hat gewählt: " + answer );
+
+    console.log("Room: " + this.roomId + " | User: " + username + " hat gewählt: " + answer);
     console.log(this.currentRightAnswer + " Richtige Antwort ");
     console.log(this.players);
-    if(this.currentRightAnswer === String(answer)){
+    if (this.currentRightAnswer === String(answer)) {
       console.log("antwort richtig");
       this.players.forEach((player) => {
         console.log("player.username " + player.username);
         console.log("player.score" + player.score);
-        if(player.username === username){
+        if (player.username === username) {
           player.score++;
           console.log("Right Answer " + "Score of " + player.username + "incremented!" + player.score);
         }
@@ -70,7 +71,7 @@ class QuizGame {
       this.players.forEach((player) => {
         console.log("player.username " + player.username);
         console.log("player.score" + player.score);
-        if(player.username !== username){
+        if (player.username !== username) {
           player.score++;
           console.log("Wrong Answer " + "Score of " + player.username + "incremented!" + player.score);
         }
@@ -78,32 +79,54 @@ class QuizGame {
     }
 
     this.round++;
-    if(this.round < 6){
+    if (this.round < 6) {
       this.newQuestion();
-    }else{
+    } else {
       console.log("Spiel zuende");
     }
-    
+
   }
 
   endGame() {
     this.players.forEach((player) => {
       this.io.to(player.id).emit('gameEnd', { score: player.score });
     });
+
+    this.players.forEach((player) => {
+      db.then(conn => {
+        conn.query("SELECT HIGHSCORE FROM USER WHERE USERNAME = ?", [player.username])
+          .then(rows => {
+            try {
+              const highscore = rows[0].HIGHSCORE
+              if (player.highscore > highscore) {
+                conn.query("UPDATE USER SET HIGHSCORE = ? WHERE USERNAME = ?", [highscore, player.username])
+                  .then(rows => {
+                    console.log(rows);
+                  })
+              }
+
+            } catch (error) {
+              console.log(error);
+            }
+          })
+      })
+    });
+
+
   }
-  newQuestion(){
+  newQuestion() {
     getNextQuestion().then(question => {
       console.log("Runde :" + this.round + " " + String(question.text));
-      
-      if(checkDuplicateQuestion(question.id)){
+
+      if (checkDuplicateQuestion(question.id)) {
         this.questions[this.round] = question.id;
         this.currentQuestionIndex = question.id;
         this.currentRightAnswer = question.right_answer;
         this.sendQuestion(question);
       }
-      else{
+      else {
         this.newQuestion();
-       }    
+      }
     }).catch(error => {
       console.log('Fehler beim Abrufen der Frage:', error);
     });
@@ -121,7 +144,7 @@ async function getNextQuestion() {
 
     if (rows.length > 0) {
       const questionRow = rows[0]; // Nehmen Sie die erste Zeile als Frage an
-      
+
       const question = {
         id: questionRow.QUESTION_ID,
         text: questionRow.QUESTION,
@@ -144,12 +167,12 @@ async function getNextQuestion() {
 }
 
 
-function checkDuplicateQuestion(quesID){
+function checkDuplicateQuestion(quesID) {
 
-  for(let i = 1; i <= this.MAX_ROUNDS; i++ ){
+  for (let i = 1; i <= this.MAX_ROUNDS; i++) {
 
-    if(questions[i] === quesID){
-      return false; 
+    if (questions[i] === quesID) {
+      return false;
     }
 
   }
