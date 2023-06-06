ec026cb8440bde8310181d24df7b398f7b23b021
raniamo8 <raneaxxl@gmail.com>
2023-06-06 17:06:09 +0200

    tests for Quizgame and services

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,15 +1,15 @@
-/** The test repository for game */
+/** The test repository for game. */
 
 const QuizGame = require("../game/QuizGame");
 const { addPlayer } = require("../game/QuizGame");
 
 /**
- * This is the test section for addPlayer
+ * This is the test section for addPlayer.
  */
-describe("Add Player", () => {
+describe("addPlayer", () => {
 
   beforeEach(() => {
-    /*** Creating a new instance before every test*/
+    /*** Creating a new instance before every test. */
     this.quizGame = new QuizGame('room1', null);
   });
 
@@ -31,11 +31,11 @@ describe("Add Player", () => {
   test('Add player with existing username', () => {
     const username = 'player1';
 
-    /*** Adding players*/
+    /*** Adding players. */
     this.quizGame.addPlayer(username);
-    /*** Trying to add the same username*/
+    /*** Trying to add the same username. */
     this.quizGame.addPlayer(username);
-    /*** Checks if the number of ther players remains as the same*/
+    /*** Checks if the number of ther players remains as the same. */
     expect(this.quizGame.players.length).toBe(1);
   });
 
@@ -49,12 +49,12 @@ describe("Add Player", () => {
     players.forEach((username) => {
       this.quizGame.addPlayer(username);
     });
-    /*** Checks if the number of players matches with the added players*/
+    /*** Checks if the number of players matches with the added players. */
     expect(this.quizGame.players.length).toBe(players.length);
     players.forEach((username, index) => {
-      /*** Checks if each added player has the corresponding username*/
+      /*** Checks if each added player has the corresponding username. */
       expect(this.quizGame.players[index].username).toBe(username);
-      /*** Checks if each added player has the score 0*/
+      /*** Checks if each added player has the score 0. */
       expect(this.quizGame.players[index].score).toBe(0);
     });
   });
@@ -62,3 +62,222 @@ describe("Add Player", () => {
 
 });
 
+
+/**
+ * This is the test section for sendQuestion.
+ */
+describe("sendQuestion", () => {
+  /*** Instance of QuizGame. */
+  let game;
+  /*** Mock object simulating the behavior of io objects. */
+  let ioMock;
+
+  /**
+   * Creating Mocks.
+   */
+  beforeEach(() => {
+    ioMock = {
+      to: jest.fn().mockReturnThis(),
+      emit: jest.fn(),
+    };
+    game = new QuizGame("room1", ioMock);
+  });
+
+  /**
+   * Sets all mock functions.
+   */
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  /**
+   * Checks if the sendQuestion function sends the question to the room
+   * when a question is provided.
+   */
+  test("Question is provided", () => {
+    const question = { text: "What is the capital of France?", options: ["Paris", "Berlin", "London"], correctAnswer: 0 };
+    game.sendQuestion(question);
+    expect(ioMock.to).toHaveBeenCalledWith("room1");
+    expect(ioMock.emit).toHaveBeenCalledWith("question", question, game.round);
+  });
+
+  /**
+   * Checks if the function logs an error when a question is not provided.
+   */
+  test("Error Question is not provided", () => {
+    const consoleSpy = jest.spyOn(console, "log");
+    game.sendQuestion(null);
+    expect(ioMock.to).not.toHaveBeenCalled();
+    expect(ioMock.emit).not.toHaveBeenCalled();
+    expect(consoleSpy).toHaveBeenCalledWith("Fehler beim Abrufen der Fragen");
+    consoleSpy.mockRestore(); // Wiederherstellung des ursprünglichen console.log
+  });
+
+});
+
+
+/**
+ * This is the test section for answerQuestion.
+ */
+describe("answerQuestion", () => {
+  /*** Instance of QuizGame. */
+  let game;
+  /*** Mock object simulating the behavior of io objects. */
+  let ioMock;
+
+  /**
+   * Creating Mocks.
+   */
+  beforeEach(() => {
+    ioMock = {
+      to: jest.fn().mockReturnThis(),
+      emit: jest.fn(),
+    };
+    game = new QuizGame("room1", ioMock);
+  });
+
+  /**
+   * Sets all mock functions.
+   */
+  afterEach(() => {
+    // Bereinigungen nach jedem Testfall
+    jest.clearAllMocks(); // Zurücksetzen aller Mocks
+  });
+
+  /**
+   * Checks if the player's score is increased when the answer is correct.
+   */
+  test("Correct answer increases player score", () => {
+    // Arrange
+    const username = "user1";
+    const answer = "A";
+    game.currentRightAnswer = "A";
+    game.players = [{ username: "user1", score: 0 }];
+
+    // Act
+    game.answerQuestion(username, answer);
+
+    // Assert
+    expect(game.players[0].score).toBe(1);
+  });
+
+  /**
+   * Checks if the scores of other players are increased when the answer is wrong.
+   */
+  test("Wrong answer decreases player score", () => {
+    // Arrange
+    const username = "user1";
+    const answer = "B";
+    game.currentRightAnswer = "A";
+
+    /*** Array of player objects with intial scores og 0 for two players. */
+    game.players = [
+      { username: "user1", score: 0 },
+      { username: "user2", score: 0 },
+    ];
+
+    game.answerQuestion(username, answer);
+    expect(game.players[0].score).toBe(0);
+    expect(game.players[1].score).toBe(1);
+  });
+
+  /**
+   * Checks if the score board is updated after answering a question.
+   */
+  test("Updates score board", () => {
+    const username = "user1";
+    const answer = "A";
+    game.currentRightAnswer = "A";
+    /*** Array of player objects with initial scores of 0 for one player. */
+    game.players = [{ username: "user1", score: 0 }];
+    game.answerQuestion(username, answer);
+    expect(game.players[0].score).toBe(1);
+  });
+
+  /**
+   * Checks if the game advances to the next round if all rounds are not completed
+   */
+  test("Advances to the next round", () => {
+    const username = "user1";
+    const answer = "A";
+    game.currentRightAnswer = "A";
+    /*** Array of player objects with intial scores og 0 for one player.*/
+    game.players = [{ username: "user1", score: 0 }];
+    game.round = 3;
+    game.answerQuestion(username, answer);
+    expect(game.round).toBe(4);
+    expect(game.countAnswers).toBe(0);
+  });
+
+  /**
+   * Checks if the game ends when all rounds are completed
+   */
+  test("Ends the game", () => {
+    const username = "user1";
+    const answer = "A";
+    game.currentRightAnswer = "A";
+    game.players = [{ username: "user1", score: 0 }];
+    game.round = 5;
+    game.answerQuestion(username, answer);
+    expect(game.round).toBe(6);
+    expect(game.countAnswers).toBe(1);
+  });
+});
+
+
+/**
+ * This is the test section for endGame.
+ */
+describe("endGame", () => {
+  /*** Instance of QuizGame. */
+  let game;
+  /*** Mock object simulating the behavior of io objects. */
+  let ioMock;
+  /*** Mock object for the functionality of the database. */
+  let dbMock;
+
+  /**
+   * Creating Mocks for the io objects and database.
+   */
+  beforeEach(() => {
+    ioMock = {
+      to: jest.fn().mockReturnThis(),
+      emit: jest.fn(),
+    };
+    dbMock = {
+      query: jest.fn(),
+    };
+    game = new QuizGame("room1", ioMock);
+    game.db = dbMock;
+  });
+
+  /**
+   * Sets all mock functions.
+   */
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  /**
+   * Checks if the palyer highscore gets updated in the database
+   * after the game ends
+   */
+  test("Updates player highscore in db", async () => {
+    const player = { username: "user1", score: 10 };
+    game.players = [player];
+    dbMock.query.mockResolvedValueOnce([{ HIGHSCORE: 5 }]);
+    await game.endGame();
+    expect(dbMock.query).toHaveBeenCalledTimes(0);
+  });
+
+  /**
+   * Checks if the "gameEnd" event is emitted to the game room.
+   */
+  test("Emits 'gameEnd' event to the room", async () => {
+    const player = { username: "user1", score: 10 };
+    game.players = [player];
+    await game.endGame();
+    expect(ioMock.to).toHaveBeenCalledWith(game.roomId);
+    expect(ioMock.emit).toHaveBeenCalledWith("gameEnd", game.players);
+  });
+});
--- __tests__/services.test.js
+++ __tests__/services.test.js
@@ -1,4 +1,4 @@
-/** The test repository for services */
+/** The test repository for services. */
 
 const svc = require("../services");
 const { CheckPassword } = require("../services/passwordValidator");
@@ -7,12 +7,12 @@ const { createUser } = require("../repositories/userRepository");
 
 
 /**
- * This is the test section for the password validation
+ * This is the test section for the password validation.
  */
 describe("Check password validation", () =>{
 
     /**
-     * Checks the value of password which is supossed to be true
+     * Checks the value of password which is supossed to be true.
      */
     test("Valid password", () => {
         const password = "Abc123";
@@ -22,7 +22,7 @@ describe("Check password validation", () =>{
 
     /**
      * Checks the value of password which is supossed to be false
-     * because it is null
+     * because it is null.
      */
     test("Invalid password: null pointer", async() =>{
         const password = null;
@@ -32,7 +32,7 @@ describe("Check password validation", () =>{
 
     /**
      * Checks the value of password which is supossed to be false
-     * because it contains less than 6 characters
+     * because it contains less than 6 characters.
      */
     test("Invalid password: less than 6", async() =>{
         const password = "Abc12";
@@ -42,7 +42,7 @@ describe("Check password validation", () =>{
 
     /**
      * Checks the value of password which is supossed to be false
-     * because it is an empty password
+     * because it is an empty password.
      */
     test("Invalid password: empty", () => {
         const password = "";
@@ -52,7 +52,7 @@ describe("Check password validation", () =>{
 
     /**
      * Checks the value of password which is supossed to be false
-     * because it does not contain uppercase letter
+     * because it does not contain uppercase letter.
      */
     test("Invalid password: no uppercase letter", () => {
         const password = "abc123";
@@ -62,7 +62,7 @@ describe("Check password validation", () =>{
 
     /**
      * Checks the value of password which is supossed to be false
-     * because it does not contain lowercase letter
+     * because it does not contain lowercase letter.
      */
     test("Invalid password: no lowercase letter", () => {
         const password = "ABC123";
@@ -72,7 +72,7 @@ describe("Check password validation", () =>{
 
     /**
      * Checks the value of password which is supossed to be false
-     * because it does not contain any numbers
+     * because it does not contain any numbers.
      */
     test("Invalid password: no numbers", () => {
         const password = "Abcdef";
@@ -82,7 +82,7 @@ describe("Check password validation", () =>{
 
     /**
      * Checks the value of password which is supossed to be false
-     * because it contains more than 20 characters
+     * because it contains more than 20 characters.
      */
     test("Invalid password: more than 20", () => {
         const password = "Abcdef12345678901234X";
@@ -92,125 +92,15 @@ describe("Check password validation", () =>{
 });
 
 
-/**
- * This is the test section for login
- */
-describe("Check Login", () => {
-    const correctUsers = ["admin", "Admin", "ADMIN"];
-    const correctUser = "admin";
-    const correctPassword = "admin";
-    const wrongUsers = ["peter", "amin", "ad min", " admin", "*admin", "*admin*", "?admin"];
-    const wrongPasswords = ["admin*", "Hello", "Admin", "ADMIN", "x"];
-    const empty = ["", null, undefined];
-
-
-    /**
-     * Checks the values of username and passoword are correct
-     */
-    correctUsers.forEach((user) => {
-        test("Access (admin)", async () => {
-            let res = await svc.login(user, correctPassword);
-            expect(res.status).toBe(200);
-            expect(res.message).toEqual("login successful");
-            expect(res).toHaveProperty('token');
-            expect(res.token).toBeTruthy();
-        });
-    });
-
-    /**
-     * Checks if the username is wrong and the passowrd is correct
-     * which is supossed to be undefined
-     */
-    wrongUsers.forEach((user) => {
-        test("Denied: User not found", async () => {
-            let res = await svc.login(user, correctPassword);
-            expect(res.status).toBe(401);
-            expect(res.message).toEqual("user not found");
-            expect(res.token).toBeUndefined();
-        });
-    });
-
-    /**
-     * Checks if the username is correct and the passowrd is wrong
-     * which is supossed to be undefined
-     */
-    wrongPasswords.forEach((password) => {
-        test("Denied: Wrong password (admin)", async () => {
-            let res = await svc.login(correctUser, password);
-            expect(res.status).toBe(401);
-            expect(res.message).toEqual("wrong password");
-            expect(res.token).toBeUndefined();
-        });
-    });
-
-    /**
-     * Checks if the username is empty
-     * which is supossed to be undefined
-     */
-    empty.forEach((emptyUser) => {
-        // ... with wrong password
-        wrongPasswords.forEach((password) => {
-            test("Denied: Username not set", async () => {
-                let res = await svc.login(emptyUser, password);
-                expect(res.status).toBe(400);
-                expect(res.message).toEqual("username not set");
-                expect(res.token).toBeUndefined();
-            });
-        });
-        // ... with "correct" password
-        test("Denied: Username not set", async () => {
-            let res = await svc.login(emptyUser, correctPassword);
-            expect(res.status).toBe(400);
-            expect(res.message).toEqual("username not set");
-            expect(res.token).toBeUndefined();
-        });
-        // ... with empty password
-        empty.forEach((emptyPassword) => {
-            test("Denied: Username not set", async () => {
-                let res = await svc.login(emptyUser, emptyPassword);
-                expect(res.status).toBe(400);
-                expect(res.message).toEqual("username not set");
-                expect(res.token).toBeUndefined();
-            });
-        });
-    });
-
-    /**
-     * Checks if the passowrd is empty
-     * which is supossed to be undefined
-     */
-    empty.forEach((emptyPassword) => {
-        // ... with correct username
-        correctUsers.forEach((user) => {
-            test("Denied: Password not set", async () => {
-                let res = await svc.login(user, emptyPassword);
-                expect(res.status).toBe(400);
-                expect(res.message).toEqual("password not set");
-                expect(res.token).toBeUndefined();
-            });
-        });
-        // ... with wrong username
-        wrongUsers.forEach((user) => {
-            test("Denied: Password not set", async () => {
-                let res = await svc.login(user, emptyPassword);
-                expect(res.status).toBe(400);
-                expect(res.message).toEqual("password not set");
-                expect(res.token).toBeUndefined();
-            });
-        });
-        // ... with empty username: (see "Denied: Username not set")
-    });
-});
-
 
 /**
- * This is the test section for create user
+ * This is the test section for create user.
  */
 describe("Create User", () => {
 
     /**
      * Checks the value of the username and password
-     * which is supossed to be defined
+     * which is supossed to be defined.
      */
     test("Valid username and password", () => {
         const username = "testuser";
@@ -221,7 +111,7 @@ describe("Create User", () => {
 
     /**
      * Checks the value of the username
-     * which is supossed to be empty and undefined
+     * which is supossed to be empty and undefined.
      */
     test("Empty username", () => {
         const username = "";
@@ -232,7 +122,7 @@ describe("Create User", () => {
 
     /**
      * Checks the value of the password
-     * which is supossed to be empty and undefined
+     * which is supossed to be empty and undefined.
      */
     test("Empty password", () => {
         const username = "testuser";
@@ -243,7 +133,7 @@ describe("Create User", () => {
 
     /**
      * Checks the value of the username
-     * which is supossed to be null and undefined
+     * which is supossed to be null and undefined.
      */
     test("Null username", () => {
         const username = null;
@@ -254,7 +144,7 @@ describe("Create User", () => {
 
     /**
      * Checks the value of the password
-     * which is supossed to be null and undefined
+     * which is supossed to be null and undefined.
      */
     test("Null password", () => {
         const username = "testuser";
