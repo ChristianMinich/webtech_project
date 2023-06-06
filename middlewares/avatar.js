76bafd241cb92b7c628bdfac5275c1c93212bbe0
ChristianMinich <119011619+ChristianMinich@users.noreply.github.com>
2023-06-06 15:56:28 +0200

    fixed avatar mw

--- middlewares/avatar.js
+++ middlewares/avatar.js
@@ -4,28 +4,32 @@ const path = require("path");
 
 const db = database.getConnection();
 
-function getAvatar(req, res, next){
-    const accessToken = req.cookies["accessToken"];
+function getAvatar(req, res, next) {
+  const accessToken = req.cookies["accessToken"];
 
-    try {
-        const decoded = svc.getData(accessToken);
+  try {
+    const decoded = svc.getData(accessToken);
 
-        db.then(conn => {
-            conn.query("SELECT A.FILE_PATH FROM USER U JOIN AVATAR A ON U.AVATAR_ID = A.AVATAR_ID WHERE U.USERNAME = ?", [decoded.username])
-            .then(rows => {
-              const avatar = rows[0].FILE_PATH
-              //console.log(avatar);
-              const avatarPath = "/assets/emojis/" + avatar;
-              req.avatar = avatarPath;
-              next();
-            })
-          });
-
-      } catch (error) {
-        console.log(error);
-      }
-
+    db.then((conn) => {
+      conn
+        .query(
+          "SELECT A.FILE_PATH FROM USER U JOIN AVATAR A ON U.AVATAR_ID = A.AVATAR_ID WHERE U.USERNAME = ?",
+          [decoded.username]
+        )
+        .then((rows) => {
+          try {
+            const avatar = rows[0].FILE_PATH;
+            const avatarPath = "/assets/emojis/" + avatar;
+            req.avatar = avatarPath;
+            next();
+          } catch (error) {
+            console.log(error);
+          }
+        });
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = { getAvatar };
-
