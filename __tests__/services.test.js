d928c3cd6acb174b33d9c80ef9659acc6319eb2c
raniamo8 <raneaxxl@gmail.com>
2023-06-06 12:43:03 +0200

    tests of create user

--- __tests__/services.test.js
+++ __tests__/services.test.js
@@ -2,9 +2,12 @@
 
 const svc = require("../services");
 const { CheckPassword } = require("../services/passwordValidator");
+const { login } = require("../services");
+const { createUser } = require("../repositories/userRepository");
+
 
 /**
- * This is the test section for the password
+ * This is the test section for the password validation
  */
 describe("Check password validation", () =>{
 
@@ -85,6 +88,164 @@ describe("Check password validation", () =>{
         const password = "Abcdef12345678901234X";
         const result = CheckPassword(password);
         expect(result).toBe(false);
+    });
+});
+
+
+/**
+ * This is the test section for login
+ */
+describe("Check Login", () => {
+    const correctUsers = ["admin", "Admin", "ADMIN"];
+    const correctUser = "admin";
+    const correctPassword = "admin";
+    const wrongUsers = ["peter", "amin", "ad min", " admin", "*admin", "*admin*", "?admin"];
+    const wrongPasswords = ["admin*", "Hello", "Admin", "ADMIN", "x"];
+    const empty = ["", null, undefined];
+
+
+    // Correct username & password
+    correctUsers.forEach((user) => {
+        test("Access (admin)", async () => {
+            let res = await svc.login(user, correctPassword);
+            expect(res.status).toBe(200);
+            expect(res.message).toEqual("login successful");
+            expect(res).toHaveProperty('token');
+            expect(res.token).toBeTruthy();
+        });
+    });
+
+    // Wrong username & "correct" password
+    wrongUsers.forEach((user) => {
+        test("Denied: User not found", async () => {
+            let res = await svc.login(user, correctPassword);
+            expect(res.status).toBe(401);
+            expect(res.message).toEqual("user not found");
+            expect(res.token).toBeUndefined();
+        });
+    });
+
+    // Wrong password & correct username
+    wrongPasswords.forEach((password) => {
+        test("Denied: Wrong password (admin)", async () => {
+            let res = await svc.login(correctUser, password);
+            expect(res.status).toBe(401);
+            expect(res.message).toEqual("wrong password");
+            expect(res.token).toBeUndefined();
+        });
+    });
+    // Empty username ...
+    empty.forEach((emptyUser) => {
+        // ... with wrong password
+        wrongPasswords.forEach((password) => {
+            test("Denied: Username not set", async () => {
+                let res = await svc.login(emptyUser, password);
+                expect(res.status).toBe(400);
+                expect(res.message).toEqual("username not set");
+                expect(res.token).toBeUndefined();
+            });
+        });
+        // ... with "correct" password
+        test("Denied: Username not set", async () => {
+            let res = await svc.login(emptyUser, correctPassword);
+            expect(res.status).toBe(400);
+            expect(res.message).toEqual("username not set");
+            expect(res.token).toBeUndefined();
+        });
+        // ... with empty password
+        empty.forEach((emptyPassword) => {
+            test("Denied: Username not set", async () => {
+                let res = await svc.login(emptyUser, emptyPassword);
+                expect(res.status).toBe(400);
+                expect(res.message).toEqual("username not set");
+                expect(res.token).toBeUndefined();
+            });
+        });
+    });
+
+    // Empty password ...
+    empty.forEach((emptyPassword) => {
+        // ... with correct username
+        correctUsers.forEach((user) => {
+            test("Denied: Password not set", async () => {
+                let res = await svc.login(user, emptyPassword);
+                expect(res.status).toBe(400);
+                expect(res.message).toEqual("password not set");
+                expect(res.token).toBeUndefined();
+            });
+        });
+        // ... with wrong username
+        wrongUsers.forEach((user) => {
+            test("Denied: Password not set", async () => {
+                let res = await svc.login(user, emptyPassword);
+                expect(res.status).toBe(400);
+                expect(res.message).toEqual("password not set");
+                expect(res.token).toBeUndefined();
+            });
+        });
+        // ... with empty username: (see "Denied: Username not set")
+    });
+});
+
+
+/**
+ * This is the test section for create user
+ */
+describe("Create User", () => {
+
+    /**
+     * Checks the value of the username and password
+     * which is supossed to be defined
+     */
+    test("Valid username and password", () => {
+        const username = "testuser";
+        const password = "Password123";
+        const result = createUser(username, password);
+        expect(result).toBeUndefined();
+    });
+
+    /**
+     * Checks the value of the username
+     * which is supossed to be empty and undefined
+     */
+    test("Empty username", () => {
+        const username = "";
+        const password = "Password123";
+        const result = createUser(username, password);
+        expect(result).toBeUndefined();
+    });
+
+    /**
+     * Checks the value of the password
+     * which is supossed to be empty and undefined
+     */
+    test("Empty password", () => {
+        const username = "testuser";
+        const password = "";
+        const result = createUser(username, password);
+        expect(result).toBeUndefined();
+    });
+
+    /**
+     * Checks the value of the username
+     * which is supossed to be null and undefined
+     */
+    test("Null username", () => {
+        const username = null;
+        const password = "Password123";
+        const result = createUser(username, password);
+        expect(result).toBeUndefined();
+    });
+
+    /**
+     * Checks the value of the password
+     * which is supossed to be null and undefined
+     */
+    test("Null password", () => {
+        const username = "testuser";
+        const password = null;
+        const result = createUser(username, password);
+        expect(result).toBeUndefined();
     });
 });
 
