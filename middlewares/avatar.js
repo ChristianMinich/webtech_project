5177daf78e50ab60e9fb7b05aab55b9c2fea3b68
ranea <rania.mohammad@hs-osnabrueck.de>
2023-06-07 11:12:50 +0200

    documentation

--- middlewares/avatar.js
+++ middlewares/avatar.js
@@ -5,10 +5,11 @@ const path = require("path");
 const db = database.getConnection();
 
 /**
+ * Middleware function to retrieve the avatar file path for a user.
  *
- * @param req
- * @param res
- * @param next
+ * @param req - Express request object.
+ * @param res - Express respond object.
+ * @param next - Next function for in the middleware chain.
  */
 function getAvatar(req, res, next) {
   const accessToken = req.cookies["accessToken"];
