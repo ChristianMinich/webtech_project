fd6b83122f97cb4b71eeaef089ac02391ed3c5b0
ChristianMinich <119011619+ChristianMinich@users.noreply.github.com>
2023-06-10 15:16:45 +0200

    refactored services

--- services/passwordValidator.js
+++ services/passwordValidator.js
@@ -2,13 +2,13 @@
  * To check a password between 6 to 20 characters which
  * contain at least one numeric digit, one uppercase and
  * one lowercase letter.
- * 
+ *
  * @param {*} inputtxt - The input text.
  * @returns {boolean} - True if the password matches, false if it does not
  */
 function CheckPassword(inputtxt) {
   var passw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
-  if (inputtxt!=null && inputtxt.match(passw)) {
+  if (inputtxt != null && inputtxt.match(passw)) {
     return true;
   } else {
     return false;
